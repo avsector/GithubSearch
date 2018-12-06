5ab6aac9d09cb3275d90b5d4e7ce7a49fbfe1041
Hamid Moazzami <mblcdr@gmail.com>
2018-12-06 20:40:12 +0330

    Add the initial implementation of search and repository row view

---
+++ app/src/main/java/com/samsaz/githubsearch/search/SearchActivity.kt
@@ -0,0 +1,19 @@
+package com.samsaz.githubsearch.search
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.samsaz.githubsearch.R
+
+
+/**
+ * Copyright 2018
+ * Created and maintained by Hamid Moazzami
+ */
+
+class SearchActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
